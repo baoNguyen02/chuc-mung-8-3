5d049400423aa8bdc81e22b68825a1361958fc66
Nguyễn Văn Bảo Nguyên <105842307+baoNguyen02@users.noreply.github.com>
2023-03-07 16:28:35 +0700

    Add files via upload

---
+++ Mung8_3/config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em yêu <3';
+const giftUrl = 'love.html';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nữa!';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
